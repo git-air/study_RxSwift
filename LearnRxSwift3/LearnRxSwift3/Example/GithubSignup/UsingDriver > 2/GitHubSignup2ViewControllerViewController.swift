1c9cd46f30b5041ad28353d90e51c5f9f80c59bb
AIR <46517472+git-air@users.noreply.github.com>
2022-08-09 03:21:00 +0900

    2022/08/09

--- LearnRxSwift3/LearnRxSwift3/Example/GithubSignup/UsingDriver > 2/GitHubSignup2ViewControllerViewController.swift
+++ LearnRxSwift3/LearnRxSwift3/Example/GithubSignup/UsingDriver > 2/GitHubSignup2ViewControllerViewController.swift
@@ -6,13 +6,52 @@
 //
 
 import UIKit
+import RxSwift
 
 class GitHubSignup2ViewControllerViewController: UIViewController {
-
+    @IBOutlet weak var usernameOutlet: UITextField!
+    @IBOutlet weak var usernameValidationOutlet: UILabel!
+    
+    @IBOutlet weak var passwordOutlet: UITextField!
+    @IBOutlet weak var passwordValidationOutlet: UILabel!
+    
+    @IBOutlet weak var repeatedPasswordOutlet: UITextField!
+    @IBOutlet weak var repeatedPasswordValidationOutlet: UILabel!
+    
+    @IBOutlet weak var signupOutlet: UIButton!
+    @IBOutlet weak var signingUpOutlet: UIActivityIndicatorView!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.red
+        let viewModel = GithubSignup2ViewModel(
+            input: (
+                username: usernameOutlet.rx.text.orEmpty.asDriver(),
+                password: passwordOutlet.rx.text.orEmpty.asDriver(),
+                repeatedPassword: repeatedPasswordOutlet.rx.text.orEmpty.asDriver(),
+                loginTaps: signupOutlet.rx.tap.asSignal()
+            ),
+            dependency: (
+                API: GitHubDefaultAPI.sharedAPI,
+                validationService: GitHubDefaultValidationService.sharedValidationService,
+                wireframe: DefaultWireframe.shared
+            )
+        )
+        
+        viewModel.signupEnabled
+            .drive(onNext: { [weak self] valid in
+                self?.signupOutlet.isEnabled = valid
+                self?.signupOutlet.alpha = valid ? 1.0 : 0.5
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.validatedUsername
+            .drive(usernameValidationOutlet.rx.validationResult)
+            .disposed(by: disposeBag)
+        
+        
 
         // Do any additional setup after loading the view.
     }
---
+++ LearnRxSwift3/LearnRxSwift3/Example/GithubSignup/UsingDriver > 2/GithubSignup2ViewModel.swift
@@ -0,0 +1,86 @@
+//
+//  GithubSignup2ViewModel.swift
+//  LearnRxSwift3
+//
+//  Created by AIR on 2022/08/08.
+//
+
+import RxSwift
+import RxCocoa
+
+class GithubSignup2ViewModel {
+    let validatedUsername: Driver<ValidationResult>
+    let validatedPassword: Driver<ValidationResult>
+    let validatedPasswordRepeated: Driver<ValidationResult>
+    
+    let signupEnabled: Driver<Bool>
+    
+    let signedIn: Driver<Bool>
+    
+    let signingIn: Driver<Bool>
+    
+    init(
+        input: (
+            username: Driver<String>,
+            password: Driver<String>,
+            repeatedPassword: Driver<String>,
+            loginTaps: Signal<()>
+        ),
+        dependency: (
+            API: GitHubAPI,
+            validationService: GitHubValidationService,
+            wireframe: Wireframe
+        )
+    ) {
+        let API = dependency.API
+        let validationService = dependency.validationService
+        let wireframe = dependency.wireframe
+        
+        validatedUsername = input.username
+            .flatMapLatest { username in
+                return validationService.validateUsername(username)
+                    .asDriver(onErrorJustReturn: .failed(message: "Error contacting server"))
+            }
+        
+        validatedPassword = input.password
+            .map{ password in
+                return validationService.validatePassword(password)
+            }
+        
+        validatedPasswordRepeated = Driver.combineLatest(input.password, input.repeatedPassword, resultSelector: validationService.validateRepeatedPassword)
+        
+        let signingIn = ActivityIndicator()
+        self.signingIn = signingIn.asDriver()
+        
+        let usernameAndPassword = Driver.combineLatest(input.username, input.password) { (username: $0, password: $1) }
+        
+        signedIn = input.loginTaps.withLatestFrom(usernameAndPassword)
+            .flatMapLatest { pair in
+                return API.signup(pair.username, password: pair.password)
+                    .trackActivity(signingIn)
+                    .asDriver(onErrorJustReturn: false)
+            }
+            .flatMapLatest { loggedIn -> Driver<Bool> in
+                let message = loggedIn ? "Mock: Signed in to GitHub" : "Mock: Sign in to GitHub failed"
+                return wireframe.promptFor(message, cancelAction: "OK", actions: [])
+                    .map { _ in
+                        loggedIn
+                    }
+                    .asDriver(onErrorJustReturn: false)
+            }
+        
+        signupEnabled = Driver.combineLatest(
+            validatedUsername,
+            validatedPassword,
+            validatedPasswordRepeated,
+            signingIn
+        ) { username, password, repeatPassword, signingIn in
+            username.isValid &&
+            password.isValid &&
+            repeatPassword.isValid &&
+            !signingIn
+        }
+        .distinctUntilChanged()
+            
+    }
+}
