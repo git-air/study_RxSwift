bd5e9e9f307552bdd5951728cd93341f974e70ba
AIR <46517472+git-air@users.noreply.github.com>
2022-08-13 11:22:11 +0900

    2022/08/13朝

---
+++ LearnRxSwift3/LearnRxSwift3/Example/APIWrappers/APIWrappersViewController.swift
@@ -0,0 +1,31 @@
+//
+//  APIWrappersViewController.swift
+//  LearnRxSwift3
+//
+//  Created by AIR on 2022/08/13.
+//
+
+import UIKit
+
+class APIWrappersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
