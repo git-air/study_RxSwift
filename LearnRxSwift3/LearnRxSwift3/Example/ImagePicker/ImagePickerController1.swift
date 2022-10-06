43f4d30c9be6c0ecb4df413338e72ef26087a9cb
AIR <46517472+git-air@users.noreply.github.com>
2022-10-06 18:48:55 +0900

    2022/10/06

---
+++ LearnRxSwift3/LearnRxSwift3/Example/ImagePicker/ImagePickerController1.swift
@@ -0,0 +1,74 @@
+//
+//  ImagePickerController.swift
+//  LearnRxSwift3
+//
+//  Created by AIR on 2022/09/29.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ImagePickerController1: ViewController1 {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var galleryButton: UIButton!
+    @IBOutlet weak var cropButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+
+        cameraButton.rx.tap
+            .flatMapLatest { [weak self] _ in
+                return UIImagePickerController.rx.createWithParent(self) { picker in
+                    picker.sourceType = .camera
+                    picker.allowsEditing = false
+                }
+                .flatMap { $0.rx.didFinishPickingMediaWithInfo }
+                .take(1)
+            }
+            .map { info in
+                return info[.originalImage] as? UIImage
+            }
+            .bind(to: imageView.rx.image)
+            .disposed(by: disposeBag)
+
+        galleryButton.rx.tap
+            .flatMapLatest { [weak self] _ in
+                return UIImagePickerController.rx.createWithParent(self) { picker in
+                    picker.sourceType = .photoLibrary
+                    picker.allowsEditing = false
+                }
+                .flatMap {
+                    $0.rx.didFinishPickingMediaWithInfo
+                }
+                .take(1)
+            }
+            .map { info in
+                return info[.originalImage] as? UIImage
+            }
+            .bind(to: imageView.rx.image)
+            .disposed(by: disposeBag)
+
+        cropButton.rx.tap
+            .flatMapLatest { [weak self] _ in
+                return UIImagePickerController.rx.createWithParent(self) { picker in
+                    picker.sourceType = .photoLibrary
+                    picker.allowsEditing = true
+                }
+                .flatMap { $0.rx.didFinishPickingMediaWithInfo }
+                .take(1)
+            }
+            .map { info in
+                return info[.editedImage] as? UIImage
+            }
+            .bind(to: imageView.rx.image)
+            .disposed(by: disposeBag)
+        
+    }
+    
+}
