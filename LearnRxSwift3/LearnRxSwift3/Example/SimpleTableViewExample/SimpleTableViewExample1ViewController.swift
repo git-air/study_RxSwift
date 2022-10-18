5df6513d89f23667dbe2903dc20149ae6ab34158
AIR <46517472+git-air@users.noreply.github.com>
2022-10-18 19:55:14 +0900

    SimpleTableView

--- LearnRxSwift3/LearnRxSwift3/Example/SimpleTableViewExample/SimpleTableViewExample1ViewController.swift
+++ LearnRxSwift3/LearnRxSwift3/Example/SimpleTableViewExample/SimpleTableViewExample1ViewController.swift
@@ -6,15 +6,39 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class SimpleTableViewExample1ViewController: UIViewController {
+class SimpleTableViewExample1ViewController: UIViewController1, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.gray
-
-        // Do any additional setup after loading the view.
+        let items = Observable.just(
+            (0..<20).map { "\($0)" }
+        )
+        
+        items
+            .bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { (row, element, cell) in
+                cell.textLabel?.text = "\(element) @ row \(row)"
+            }
+            .disposed(by: disposeBag)
+        
+        tableView.rx
+            .modelSelected(String.self)
+            .subscribe(onNext: { value in
+                DefaultWireframe.presentAlert("Tapped `\(value)`")
+            })
+            .disposed(by: disposeBag)
+        
+        tableView.rx
+            .itemAccessoryButtonTapped
+            .subscribe(onNext: { indexPath in
+                DefaultWireframe.presentAlert("Tapped Detail @ \(indexPath.section),\(indexPath.row)")
+            })
+            .disposed(by: disposeBag)
     }
     
 
