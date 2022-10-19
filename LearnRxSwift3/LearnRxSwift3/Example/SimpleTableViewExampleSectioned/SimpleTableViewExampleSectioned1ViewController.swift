821dab0f7f1d8acda40fd06eae047dc162dea18c
AIR <46517472+git-air@users.noreply.github.com>
2022-10-19 20:02:06 +0900

    storyboard追加

---
+++ LearnRxSwift3/LearnRxSwift3/Example/SimpleTableViewExampleSectioned/SimpleTableViewExampleSectioned1ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SimpleTableViewExampleSectioned1ViewController.swift
+//  LearnRxSwift3
+//
+//  Created by AIR on 2022/10/19.
+//
+
+import UIKit
+
+class SimpleTableViewExampleSectioned1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
